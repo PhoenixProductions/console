49e7620a25947659d95bcd5d0f8f8ec7e08315f2
Michael Hughes <michaelhughes@strath.ac.uk>
2014-03-06 15:19:51 +0000

    distribution config file

---
+++ config-dist.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$CFG = new stdClass();
+$CFG->wwwroot = 'http://192.168.56.101/~igs03102/console/';
+$CFG->dirroot = __DIR__;
+        //'/home/igs03102/Development/console';
+$CFG->user = '';
+$CFG->pass = '';
